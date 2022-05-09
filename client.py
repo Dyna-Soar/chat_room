4a4bead55f30e9bd2202d75f0640d961a0b3e6ab
Bodin <t.bodin@MacBook-Air-de-Bodin.local>
2022-05-09 23:17:15 +0200

    add broadcast and handle funcitons

--- client.py
+++ client.py
@@ -5,3 +5,28 @@ HOST = "127.0.0.1"
 PORT = 55555
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+clients = []
+nicknames = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat'.encode("ascii"))
+            nicknames.remove(nickname)
+            break
