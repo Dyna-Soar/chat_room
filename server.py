67eb191f4d836ad74efad5637e9abb234996fe84
Bodin <t.bodin@MacBook-Air-de-Bodin.local>
2022-05-10 00:05:09 +0200

    add receive and write functions

--- server.py
+++ server.py
@@ -1,2 +1,37 @@
 import socket
 import threading
+
+nickname = input("Choose a nickname: ")
+
+HOST = "127.0.0.1"
+PORT = 55554
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode("ascii")
+            if message == "NICK":
+                client.send(nickname.encode("ascii"))
+            else:
+                print(message)
+        except:
+            print(f'An error occurred!')
+            client.close()
+            break
+
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode("ascii"))
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
